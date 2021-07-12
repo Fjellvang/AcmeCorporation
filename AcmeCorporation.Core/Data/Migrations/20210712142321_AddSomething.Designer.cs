7fd1f8811036b55daad0ee4fb736d6da5c591b57
aleksander fjellvang <asf@appstract.dk>
2021-07-12 16:23:58 +0200

    added namechanging migrationg

--- AcmeCorporation.Core/Data/Migrations/20210712142321_AddSomething.Designer.cs
+++ AcmeCorporation.Core/Data/Migrations/20210712142321_AddSomething.Designer.cs
@@ -4,14 +4,16 @@ using AcmeCorporation.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AcmeCorporation.Data.Migrations
 {
     [DbContext(typeof(AcmeCorporationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210712142321_AddSomething")]
+    partial class AddSomething
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AcmeCorporation.Core/Data/Migrations/20210712142321_AddSomething.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AcmeCorporation.Data.Migrations
+{
+    public partial class AddSomething : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserSerials_UserId",
+                table: "UserSerials");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserSerials_UserId",
+                table: "UserSerials",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_UserSerials_UserId",
+                table: "UserSerials");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserSerials_UserId",
+                table: "UserSerials",
+                column: "UserId",
+                unique: true,
+                filter: "[UserId] IS NOT NULL");
+        }
+    }
+}
