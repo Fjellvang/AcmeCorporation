5b98627bb6b1598da61a31ef4488260a244477ff
aleksander fjellvang <asf@appstract.dk>
2021-07-12 11:10:41 +0200

    migrations for missing dbSets

--- AcmeCorporation.Core/Data/Migrations/20210712090938_AddDbSets.Designer.cs
+++ AcmeCorporation.Core/Data/Migrations/20210712090938_AddDbSets.Designer.cs
@@ -4,14 +4,16 @@ using AcmeCorporation.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AcmeCorporation.Data.Migrations
 {
     [DbContext(typeof(AcmeCorporationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210712090938_AddDbSets")]
+    partial class AddDbSets
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
