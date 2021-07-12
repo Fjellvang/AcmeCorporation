f5f8373b3c6935f68c75cfcce270491c2ca0508a
aleksander fjellvang <asf@appstract.dk>
2021-07-12 10:40:38 +0200

    added test proj

---
+++ AcmeCorporation.Core/Draw/Services/DrawSubmissionService.cs
@@ -0,0 +1,29 @@
+﻿using AcmeCorporation.Core.Draw.Dtos;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeCorporation.Core.Draw.Services
+{
+	public class DrawSubmissionService : IDrawSubmissionService
+	{
+		private readonly IUserStore<ApplicationIdentity> userStore;
+
+		public DrawSubmissionService(IUserStore<ApplicationIdentity> userStore)
+		{
+			this.userStore = userStore;
+		}
+		public Task<SubmissionResult> SubmitSerialAsync(DrawSubmissionView view)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<SubmissionResult> SubmitSerialAsync(string userId, Guid Serial)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ AcmeCorporation.Core/Draw/Services/IDrawSubmissionService.cs
@@ -0,0 +1,21 @@
+﻿using AcmeCorporation.Core.Draw.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeCorporation.Core.Draw.Services
+{
+	public enum SubmissionResult
+	{
+		Success,
+		InvalidSerial,
+		SerialAlreadySubmitted
+	}
+	interface IDrawSubmissionService
+	{
+		Task<SubmissionResult> SubmitSerialAsync(DrawSubmissionView view);
+		Task<SubmissionResult> SubmitSerialAsync(string userId, Guid Serial);
+	}
+}
