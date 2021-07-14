b086f5fee50853b9f9b85a44cfe65e572376b39c
aleksander fjellvang <asf@appstract.dk>
2021-07-14 09:34:40 +0200

    added and ran migration

--- AcmeCorporation.Core/Data/Migrations/20210714073353_removedUserSerialId.Designer.cs
+++ AcmeCorporation.Core/Data/Migrations/20210714073353_removedUserSerialId.Designer.cs
@@ -4,14 +4,16 @@ using AcmeCorporation.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AcmeCorporation.Core.Migrations
 {
     [DbContext(typeof(AcmeCorporationDbContext))]
-    partial class AcmeCorporationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210714073353_removedUserSerialId")]
+    partial class removedUserSerialId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ AcmeCorporation.Core/Data/Migrations/20210714073353_removedUserSerialId.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AcmeCorporation.Core.Migrations
+{
+    public partial class removedUserSerialId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "UserSerials");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "UserSerials",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
